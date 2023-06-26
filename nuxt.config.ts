6590c77e48ad49b1357a7366f79b7d40002999ba
xnorain001 <3385842328@qq.com>
2023-06-26 19:05:23 +0800

    update: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  // @ts-ignore
+  vite: {
+    server: {
+      fs: {
+        strict: false
+      }
+    }
+  }
 })
