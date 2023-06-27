1764f250815c0162101f9c9309ea14b47e528f3c
xnorain001 <3385842328@qq.com>
2023-06-27 20:39:45 +0800

    update: test slider component

--- pages/u-slider.vue
+++ pages/u-slider.vue
@@ -1,17 +1,17 @@
 <template>
-  <USlider class="w-full h-[20px]">
-    <template #default>
-      <USliderInner v-model="value" :max="1000">
-        <template #default>
-          <div class="w-[20px] h-[20px] bg-red-500 rounded-full"></div>
-        </template>
-      </USliderInner>
+  <USlider class="w-full h-[40px]" v-model="value" :max="200">
+    <template #track>
+      <div class="w-full h-[4px] bg-red-500"></div>
+    </template>
+    <template #thumb>
+        <div class="w-[20px] h-[20px] bg-red-500 rounded-full">
+        </div>
     </template>
   </USlider>
 </template>
 
 <script lang="ts" setup>
-const value = ref(0)
+const value = ref(50)
 
 watch(value, (v) => {
   console.log(v)
