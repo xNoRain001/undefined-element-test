b64455a46d189df00ca69ff0d601c3bb7fc3f92e
xnorain001 <3385842328@qq.com>
2023-06-27 21:21:45 +0800

    refactor: remove unnecessary named slots

--- pages/u-slider.vue
+++ pages/u-slider.vue
@@ -1,12 +1,13 @@
 <template>
-  <USlider class="w-full h-[40px]" v-model="value" :max="200">
-    <template #track>
-      <div class="w-full h-[4px] bg-red-500"></div>
-    </template>
-    <template #thumb>
-        <div class="w-[20px] h-[20px] bg-red-500 rounded-full">
-        </div>
-    </template>
+  <USlider class="w-full h-[40px] flex items-center">
+    <USliderTrack v-model="value" :max="200" class="w-full bg-gray-500">
+      <template #selection>
+        <div class="h-[4px] bg-green-500"></div>
+      </template>
+      <template #thumb>
+        <div class="w-[20px] h-[20px] bg-red-500 rounded-full"></div>
+      </template>
+    </USliderTrack>
   </USlider>
 </template>
 
@@ -14,6 +15,6 @@
 const value = ref(50)
 
 watch(value, (v) => {
-  console.log(v)
+  // console.log(v)
 })
 </script>
--- pages/u-tabs.vue
+++ pages/u-tabs.vue
@@ -1,38 +1,26 @@
 <template>
   <UTabs>
-    <template #default>
-      <div class="flex justify-center">
-        <UTab data-key="1" data-active="true">
-          <template #default>
-            <span style="width: 20px;">Tab1</span>
-          </template>
-        </UTab> 
-        <UTab data-key="2" class="ml-[10px]">
-          <template #default>
-            <span style="width: 20px;">Tab2</span>
-          </template>
-        </UTab> 
-      </div>
-    </template>
+    <div class="flex justify-center">
+      <UTab data-key="1" data-active="true">
+        <span style="width: 20px;">Tab1</span>
+      </UTab> 
+      <UTab data-key="2" class="ml-[10px]">
+        <span style="width: 20px;">Tab2</span>
+      </UTab> 
+    </div>
   </UTabs>
 
   <UTabPannels>
-    <template #default>
-      <div class="
-        border-[1px] border-solid border-gray-500 h-32 flex justify-center 
-        items-center
-      ">
-        <UTabPannel data-key="1">
-          <template #default>
-            <div>Tab1 panel</div>
-          </template>
-        </UTabPannel>
-        <UTabPannel data-key="2">
-          <template #default>
-            <div>Tab2 panel</div>
-          </template>
-        </UTabPannel>
-      </div>
-    </template>
+    <div class="
+      border-[1px] border-solid border-gray-500 h-32 flex justify-center 
+      items-center
+    ">
+      <UTabPannel data-key="1">
+        <div>Tab1 panel</div>
+      </UTabPannel>
+      <UTabPannel data-key="2">
+        <div>Tab2 panel</div>
+      </UTabPannel>
+    </div>
   </UTabPannels>
 </template>
