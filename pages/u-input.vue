5863ea42923d294ac59863d6a436abc5a796f07d
xnorain001 <3385842328@qq.com>
2023-06-29 15:54:11 +0800

    update: test input component debounce

--- pages/u-input.vue
+++ pages/u-input.vue
@@ -1,17 +1,49 @@
 <template>
-  <UInput class=" input-container cursor-text px-[12px] py-[16px] border-[2px] border-solid 
-    border-[#B3B7C1] rounded-[8px] flex items-center" v-model="value">
+  <UInput 
+    class="input-container"
+    v-model="value1"
+    placeholder="input your content..."
+  >
     <template #prefix>
-      <div class="text-red-500 mr-[10px]">prefix</div>
+      <div class="prefix">prefix</div>
     </template>
     <template #suffix>
       <div>suffix</div>
     </template>
   </UInput>
 
-  <div>value: {{ value }}</div>
+  <UInput 
+    class="input-container"
+    v-model="value2"
+    debounce="300"
+    placeholder="input your content...(debounced)"
+  >
+    <template #prefix>
+      <div class="prefix">prefix</div>
+    </template>
+    <template #suffix>
+      <div>suffix</div>
+    </template>
+  </UInput>
+
+  <div class="mt-4">
+    <span>value1: {{ value1 }}</span><br>
+    <span>value2: {{ value2 }}</span>
+  </div>
 </template>
 
 <script lang="ts" setup>
-const value = ref('')
+const value1 = ref('')
+const value2 = ref('')
 </script>
+
+<style type="text/tailwindcss" scoped>
+.input-container {
+  @apply px-[12px] py-[16px] border-[2px] border-solid 
+  border-[#B3B7C1] rounded-[8px] flex items-center mt-4
+}
+
+.prefix {
+  @apply mr-2 text-green-500
+}
+</style>
