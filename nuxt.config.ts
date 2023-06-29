1a7ceedcfe4ce27c4dd95abeb0adc83c9e2bca0b
xnorain001 <3385842328@qq.com>
2023-06-29 14:17:01 +0800

    chore: import style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,5 +20,9 @@ export default defineNuxtConfig({
     ]
   },
 
-  css: ['virtual:svg-icons-register', 'assets/css/main.css']
+  css: [
+    'virtual:svg-icons-register', 
+    'assets/css/main.css', 
+    'undefined-element/src/assets/css/index.css'
+  ]
 })
