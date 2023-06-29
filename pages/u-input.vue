8ba5430cd584857ef17128665a64f3cb22ca145a
xnorain001 <3385842328@qq.com>
2023-06-29 21:25:28 +0800

    update: test input component

--- pages/u-input.vue
+++ pages/u-input.vue
@@ -1,8 +1,11 @@
 <template>
   <UInput 
+    ref="inputContainer"
     class="input-container"
     v-model="value1"
     placeholder="input your content..."
+    @focus="focusHandler"
+    @blur="blurHandler"
   >
     <template #prefix>
       <SvgIcon name="location" width="24" height="24" class="prefix"></SvgIcon>
@@ -17,6 +20,8 @@
     v-model="value2"
     debounce="300"
     placeholder="input your content..."
+    @focus="focusHandler"
+    @blur="blurHandler"
   >
     <template #prefix>
       <button class="btn prefix whitespace-nowrap">undefined element</button>
@@ -58,6 +63,25 @@
     class="input-container"
     v-model="value5"
     placeholder="input your content..."
+    @focus="focusHandler"
+    @blur="blurHandler"
+    @clear="clearHandler"
+  >
+    <template #prefix>
+      <SvgIcon name="location" width="24" height="24" class="prefix"></SvgIcon>
+    </template>
+    <template #suffix>
+      <span data-clearable class="cursor-pointer">×</span>
+    </template>
+  </UInput>
+
+  <UInput 
+    class="input-container"
+    v-model="value6"
+    placeholder="input your content..."
+    autofocus
+    @focus="focusHandler"
+    @blur="blurHandler"
   >
     <template #prefix>
       <SvgIcon name="location" width="24" height="24" class="prefix"></SvgIcon>
@@ -73,24 +97,53 @@
     <span>value3: {{ value3 }}</span><br>
     <span>value4: {{ value4 }}</span><br>
     <span>value5: {{ value5 }}</span><br>
+    <span>value6: {{ value6 }}</span><br>
   </div>
 </template>
 
 <script lang="ts" setup>
 const value1 = ref('')
-const value2 = ref('debounced content...')
-const value3 = ref('readonly content...')
-const value4 = ref('disabled content...')
-const value5 = ref('clearable content...')
+const value2 = ref('debounced...')
+const value3 = ref('readonly...')
+const value4 = ref('disabled...')
+const value5 = ref('clearable...')
+const value6 = ref('autofocus...')
+
+const focusHandler = (e: Event) => {
+  // const container = (e.target as HTMLElement).parentNode as HTMLElement
+  // container.style.borderColor = '#3b82f6'
+}
+
+const blurHandler = (e: Event) => {
+  // const container = (e.target as HTMLElement).parentNode as HTMLElement
+  // container.style.borderColor = 'rgba(0,0,0,.3)'
+}
+
+const clearHandler = (oldValue: string) => {
+  console.log(oldValue)
+}
 </script>
 
 <style type="text/tailwindcss" scoped>
 .input-container {
-  @apply px-[12px] py-[16px] border-[2px] border-solid 
-  border-[#B3B7C1] rounded-[8px] flex items-center mt-4
+  @apply px-2 border-[2px] border-solid border-[rgba(0,0,0,.3)] h-14
+  rounded-md mt-4
 }
 
+.input-container:hover {
+  @apply border-[rgba(0,0,0,.6)]
+}
+
+.input-container 
 .prefix {
   @apply mr-2
+}
+
+.input-container :deep(.u-input) {
+  @apply font-normal
+}
+
+.input-container :deep(.u-input)::placeholder {
+  @apply text-[rgba(0,0,0,.3)]
 }
 </style>
