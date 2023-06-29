408c71f667d938541cbd2ea868139e5b43309d72
xnorain001 <3385842328@qq.com>
2023-06-29 13:37:12 +0800

    update: dialog component add persistent option

--- pages/u-dialog.vue
+++ pages/u-dialog.vue
@@ -5,6 +5,7 @@
     <button @click="updateVisible3" class="btn">bottom</button>
     <button @click="updateVisible4" class="btn">left</button>
     <button @click="updateVisible5" class="btn">center</button>
+    <button @click="updateVisible6" class="btn">persistent</button>
   </div>
 
   <UDialog v-model="visible1" position="top">
@@ -61,6 +62,17 @@
       <button @click="updateVisible5" class="btn">close</button>
     </div>
   </UDialog>
+
+  <UDialog v-model="visible6" position="center" persistent>
+    <div class="dialog-content-container">
+      <div class="header-container flex justify-between">
+        <div>header</div>
+        <div class="icon" @click="updateVisible6">×</div>
+      </div>
+      <div class="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eum voluptate saepe neque iure! Similique sed repudiandae sapiente maxime sequi consectetur vel, impedit exercitationem harum sunt esse, quo minima fugit.</div>
+      <button @click="updateVisible6" class="btn">close</button>
+    </div>
+  </UDialog>
 </template>
 
 <script lang="ts" setup>
@@ -69,12 +81,14 @@ const visible2 = ref(false)
 const visible3 = ref(false)
 const visible4 = ref(false)
 const visible5 = ref(false)
+const visible6 = ref(false)
 
 const updateVisible1 = () => visible1.value = !visible1.value
 const updateVisible2 = () => visible2.value = !visible2.value
 const updateVisible3 = () => visible3.value = !visible3.value
 const updateVisible4 = () => visible4.value = !visible4.value
 const updateVisible5 = () => visible5.value = !visible5.value
+const updateVisible6 = () => visible6.value = !visible6.value
 </script>
 
 <style type="tailwind/css" scoped>
