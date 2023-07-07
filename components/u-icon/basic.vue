8442ea99c0e7fe8d387c4ab6491f97fd7a291bbd
xnorain001 <3385842328@qq.com>
2023-07-07 14:05:16 +0800

    update: test icon component

--- components/u-icon/basic.vue
+++ components/u-icon/basic.vue
@@ -1,8 +1,8 @@
 <template>
   <Example id="basic">
-    <div class="w-full">
+    <div class="w-full grid gap-y-[24px]">
       <div>
-        <span>icons from </span>
+        <span>icon from </span>
         <a 
           target="_blank" 
           class="text-blue-500 hover:underline" 
@@ -11,11 +11,35 @@
           https://fonts.google.com/icons
         </a>
       </div>
-      <u-icon name="favorite" width="18" height="18" color="#1976d2"></u-icon>
-      <u-icon name="favorite" width="24" height="24" color="#1976d2"></u-icon>
-      <u-icon name="favorite" width="32" height="32" color="#1976d2"></u-icon>
-      <u-icon name="favorite" width="38" height="38" color="#1976d2"></u-icon>
-      <u-icon name="favorite" width="46" height="46" color="#1976d2"></u-icon>
+
+      <h2>Filled</h2>
+      
+      <div>
+        <u-icon name="favorite" width="18" height="18" color="#1976d2"></u-icon>
+        <u-icon name="favorite" width="24" height="24" color="#1976d2"></u-icon>
+        <u-icon name="favorite" width="32" height="32" color="#1976d2"></u-icon>
+        <u-icon name="favorite" width="38" height="38" color="#1976d2"></u-icon>
+        <u-icon name="favorite" width="46" height="46" color="#1976d2"></u-icon>
+      </div>
+
+      <div>
+        <u-icon name="trending_up" width="18" height="18" color="#1976d2"></u-icon>
+        <u-icon name="trending_up" width="24" height="24" color="#1976d2"></u-icon>
+        <u-icon name="trending_up" width="32" height="32" color="#1976d2"></u-icon>
+        <u-icon name="trending_up" width="38" height="38" color="#1976d2"></u-icon>
+        <!-- or Lower Camel Case -->
+        <u-icon name="trendingUp" width="46" height="46" color="#1976d2"></u-icon>
+      </div>
+
+      <h2>Outlined</h2>
+
+      <div>
+        <u-icon type="outlined" name="thumb_up" width="18" height="18" color="#1976d2"></u-icon>
+        <u-icon type="outlined" name="thumb_up" width="24" height="24" color="#1976d2"></u-icon>
+        <u-icon type="outlined" name="thumb_up" width="32" height="32" color="#1976d2"></u-icon>
+        <u-icon type="outlined" name="thumb_up" width="38" height="38" color="#1976d2"></u-icon>
+        <u-icon type="outlined" name="thumb_up" width="46" height="46" color="#1976d2"></u-icon>
+      </div>
     </div>
   </Example>
 </template>
