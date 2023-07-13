e03c21f55342a6cb51172abadf72e88c53df6103
xnorain001 <3385842328@qq.com>
2023-07-13 18:52:38 +0800

    update: test toggle component

---
+++ codes/u-toggle/02.icon.ts
@@ -0,0 +1,38 @@
+import splitCode from '../../utils/split-code'
+
+const code = `<template>
+  
+    <div class="w-full">
+      <u-toggle 
+        v-model="value"
+        class="w-[52px] h-[20px] px-[10px]"
+        thumbClass="
+          w-[20px] h-[20px] rounded-full bg-white left-0
+        "
+        :thumbStyle="{ 
+          boxShadow: '0 3px 1px -2px #0003, 0 2px 2px #00000024, 0 1px 5px #0000001f' 
+        }"
+        trackClass="!w-[32px] !h-[14px] rounded-full bg-gray-500 opacity-30"
+        activeThumbClass="left-[32px] !bg-green-500"
+        activeTrackClass="!bg-green-500 "
+      >
+        <u-icon 
+          :name="value ? 'done' : 'close'"
+          width="16"
+          height="16"
+          :color="value ? 'white' : 'black'"
+        ></u-icon>
+      </u-toggle>
+    </div>
+  
+</template>
+
+<script lang="ts" setup>
+const value = ref(false)
+</script>
+`
+const target = {}
+
+splitCode(code, target)
+
+export default target
--- codes/u-toggle/index.ts
+++ codes/u-toggle/index.ts
@@ -1,5 +1,7 @@
 import basic from './01.basic'
+import icon from './02.icon'
 
 export default {
 	'01.basic': basic,
+	'02.icon': icon,
 }
