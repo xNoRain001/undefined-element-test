ff7c5e6704bdd11c1964cd1a5b14d1d632a90410
xNoRain001 <3385842328@qq.com>
2023-07-13 22:55:06 +0800

    update: i18n config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n'
+  ],
+
+  // @ts-ignore
+  i18n: {
+    vueI18n: './i18n.config.ts' // if you are using custom path, default 
+  },
 
   // @ts-ignore
   vite: {
