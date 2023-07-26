a8d5827cf5b719fda39a003063b0cb038278c618
xnorain001 <3385842328@qq.com>
2023-07-26 13:48:56 +0800

    update: test checkbox component

--- components/u-checkbox/01.basic.vue
+++ components/u-checkbox/01.basic.vue
@@ -7,58 +7,74 @@
     <div class="w-full flex items-center [&>*:not(:first-of-type)]:ml-[32px]">
       <u-checkbox 
         v-model="value" 
-        class="
-          w-[20px] h-[20px] border-[2px] border-solid 
-          border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
-          items-center hover:border-red-500
-        "
-        activeClass="
-          bg-red-500 border-red-500
-        "
+        class="flex items-center"
       >
-        <u-icon name="done" color="white" width="16" height="16"></u-icon>
+        <template #checkbox="{ checked }">
+          <div 
+            class="
+              w-[20px] h-[20px] border-[2px] border-solid 
+              border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
+              items-center hover:border-red-500 duration-300 transition-[all]
+            "
+            :class="checked ? 'bg-red-500 border-red-500' : ''"
+          >
+            <Transition name="u-animate-opacity">
+              <u-icon v-if="checked" name="done" color="white" width="16" height="16"></u-icon>
+            </Transition>
+          </div>
+        </template>
+
+        <template #after>
+          <span class="ml-[8px]">label</span>
+        </template>
       </u-checkbox>
 
       <u-checkbox 
         v-model="value" 
-        class="
-          w-[20px] h-[20px] border-[2px] border-solid 
-          border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
-          items-center rounded-full
-        "
-        activeClass="
-          border-blue-500
-        "
+        class="flex items-center"
       >
-        <div class="w-[10px] h-[10px] bg-blue-500 rounded-full"></div>
+        <template #checkbox="{ checked }">
+          <div 
+            class="
+              w-[20px] h-[20px] border-[2px] border-solid rounded-full
+              border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
+              items-center hover:border-green-500 duration-300 transition-[all]
+            "
+            :class="checked ? 'border-green-500' : ''"
+          >
+            <Transition name="u-animate-opacity">
+              <div v-if="checked" class="w-[10px] h-[10px] bg-green-500 rounded-full"></div>
+            </Transition>
+          </div>
+        </template>
+
+        <template #after>
+          <span class="ml-[8px]">label</span>
+        </template>
       </u-checkbox>
 
       <u-checkbox 
         v-model="value" 
-        class="
-          w-[20px] h-[20px] border-[2px] border-solid 
-          border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
-          items-center
-        "
-        activeClass="
-          bg-green-500 border-green-500
-        "
+        class="flex items-center"
       >
-        <u-icon name="done" color="white" width="16" height="16"></u-icon>
-      </u-checkbox>
+        <template #checkbox="{ checked }">
+          <div 
+            class="
+              w-[20px] h-[20px] border-[2px] border-solid rounded-full
+              border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
+              items-center hover:border-blue-500 duration-300 transition-[all]
+            "
+            :class="checked ? 'bg-blue-500 border-blue-500' : ''"
+          >
+            <Transition name="u-animate-opacity">
+              <u-icon v-if="checked" name="done" color="white" width="16" height="16"></u-icon>
+            </Transition>
+          </div>
+        </template>
 
-      <u-checkbox 
-        v-model="value" 
-        class="
-          w-[20px] h-[20px] border-[2px] border-solid 
-          border-[rgba(0,0,0,.54)] rounded-full flex justify-center
-          items-center
-        "
-        activeClass="
-          bg-pink-500 border-pink-500
-        "
-      >
-        <u-icon name="done" color="white" width="16" height="16"></u-icon>
+        <template #before>
+          <span class="mr-[8px]">label</span>
+        </template>
       </u-checkbox>
     </div>
   </Example>
--- components/u-checkbox/02.group.vue
+++ components/u-checkbox/02.group.vue
@@ -9,60 +9,55 @@
         v-model="selection"
         class="flex items-center [&>*:not(:first-of-type)]:ml-[32px]"
       >
-        <u-checkbox 
-          value="red"
-          class="
-            w-[20px] h-[20px] border-[2px] border-solid 
-            border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
-            items-center
-          "
-          activeClass="
-            bg-red-500 border-red-500
-          "
-        >
-          <u-icon name="done" color="white" width="16" height="16"></u-icon>
+        <u-checkbox class="flex items-center" value="red">
+          <template #checkbox="{ checked }">
+            <div 
+              class="
+                w-[20px] h-[20px] border-[2px] border-solid 
+                border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
+                items-center hover:border-red-500 duration-300 transition-[all]
+              "
+              :class="checked ? 'bg-red-500 border-red-500' : ''"
+            >
+              <Transition name="u-animate-opacity">
+                <u-icon v-if="checked" name="done" color="white" width="16" height="16"></u-icon>
+              </Transition>
+            </div>
+          </template>
         </u-checkbox>
 
-        <u-checkbox 
-          value="blue"
-          class="
-            w-[20px] h-[20px] border-[2px] border-solid 
-            border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
-            items-center
-          "
-          activeClass="
-            bg-blue-500 border-blue-500
-          "
-        >
-          <u-icon name="done" color="white" width="16" height="16"></u-icon>
+        <u-checkbox class="flex items-center" value="green">
+          <template #checkbox="{ checked }">
+            <div 
+              class="
+                w-[20px] h-[20px] border-[2px] border-solid 
+                border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
+                items-center hover:border-green-500 duration-300 transition-[all]
+              "
+              :class="checked ? 'bg-green-500 border-green-500' : ''"
+            >
+              <Transition name="u-animate-opacity">
+                <u-icon v-if="checked" name="done" color="white" width="16" height="16"></u-icon>
+              </Transition>
+            </div>
+          </template>
         </u-checkbox>
 
-        <u-checkbox 
-          value="green"
-          class="
-            w-[20px] h-[20px] border-[2px] border-solid 
-            border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
-            items-center
-          "
-          activeClass="
-            bg-green-500 border-green-500
-          "
-        >
-          <u-icon name="done" color="white" width="16" height="16"></u-icon>
-        </u-checkbox>
-
-        <u-checkbox 
-          value="pink"
-          class="
-            w-[20px] h-[20px] border-[2px] border-solid 
-            border-[rgba(0,0,0,.54)] rounded-full flex justify-center
-            items-center
-          "
-          activeClass="
-            bg-pink-500 border-pink-500
-          "
-        >
-          <u-icon name="done" color="white" width="16" height="16"></u-icon>
+        <u-checkbox class="flex items-center" value="blue">
+          <template #checkbox="{ checked }">
+            <div 
+              class="
+                w-[20px] h-[20px] border-[2px] border-solid 
+                border-[rgba(0,0,0,.54)] rounded-[2px] flex justify-center
+                items-center hover:border-blue-500 duration-300 transition-[all]
+              "
+              :class="checked ? 'bg-blue-500 border-blue-500' : ''"
+            >
+              <Transition name="u-animate-opacity">
+                <u-icon v-if="checked" name="done" color="white" width="16" height="16"></u-icon>
+              </Transition>
+            </div>
+          </template>
         </u-checkbox>
       </u-checkbox-group>
 
